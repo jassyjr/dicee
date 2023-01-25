f0b4c96227e6a9f62995febda8c73396993d711b
jassyjr <116488124+jassyjr@users.noreply.github.com>
2023-01-25 14:52:42 +0300

    Update index.js
    ok

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images" + randomDiceImage; //images/dice1.png - images/dice6.png
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
+image.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "dice" + randomNumber2 + ".png";
+var randomImageSource = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1]
-.setAttribute("src", randomImageSource2);
+.setAttribute("src", randomImageSource);
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩" + player1 +"Wins!";
 }
@@ -29,20 +29,20 @@ document.getElementById("btn2").addEventListener("click",function roll() {
   
   var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
   
-  var randomDiceImage = "images/dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
+  var randomDiceImage = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
   
   var randomImageSource = randomDiceImage; //images/dice1.png - images/dice6.png
   
-  var image2 = document.querySelectorAll("img")[0];
+  var image = document.querySelectorAll("img")[0];
   
   image2.setAttribute("src", randomImageSource);
   
   
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
   
-  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  var randomImageSource= "dice" + randomNumber2 + ".png";
   
-  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
   
   
   //If player 1 wins
